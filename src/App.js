b054b2076324302889943705e62386b36352575a
Ryan Delos Santos <delossantosrryan@gmail.com>
2023-10-01 17:47:21 -0700

    complete lesson 1.1 assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
+const todoList = [
+  {
+    objectID: 0,
+    title: 'complete lesson 1',
+  },
+  {
+    objectID: 1,
+    title: 'workout',
+  },
+  {
+    objectID: 2,
+    title: 'laundry',
+  },
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading">Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.objectID}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
