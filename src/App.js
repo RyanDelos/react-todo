42c84a645ed40c4bc5d7356d2d2316a8f01eaa23
Ryan Delos Santos <delossantosrryan@gmail.com>
2023-11-27 08:57:41 -0800

    lesson_1_8

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
+const apiUrl = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const fetchData = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer${process.env.REACT_APP_AIRTABLE_API_TOKEN}}`,
+      },
+    };
+    try {
+      const response = await fetch(apiUrl, options);
+
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const todoFromAPI = await response.json();
+      console.log('todoFromAPI:', todoFromAPI);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   useEffect(() => {
-    new Promise((resolve) =>
-      setTimeout(
-        () =>
-          resolve({
-            data: {
-              todoList: JSON.parse(localStorage.getItem('savedTodoList')),
-            },
-          }),
-        2000
-      )
-    ).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
+    fetchData();
+    // new Promise((resolve) =>
+    //   setTimeout(
+    //     () =>
+    //       resolve({
+    //         data: {
+    //           todoList: JSON.parse(localStorage.getItem('savedTodoList')),
+    //         },
+    //       }),
+    //     2000
+    //   )
+    // ).then((result) => {
+    //   setTodoList(result.data.todoList);
+    //   setIsLoading(false);
+    // });
   }, []);
 
   useEffect(() => {
