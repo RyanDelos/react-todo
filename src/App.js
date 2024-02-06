9933899e084231e0238e1a8bddf7571fd02b8026
Ryan Delos Santos <delossantosrryan@gmail.com>
2024-02-05 16:55:27 -0800

    lesson 5-1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import AddTodoForm from './components/AddTodoForm';
 import TodoList from './components/TodoList';
+import Loading from './components/Loading';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import style from './components/TodoListItem.module.css';
+import { object } from 'prop-types';
 
 const BASE_URL = 'https://api.airtable.com/v0';
 const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
 const TABLE_NAME = process.env.REACT_APP_TABLE_NAME;
+const GRID_VIEW = 'view=Grid%20view';
+const SORT_ASC = '&sort[0][field]=title&sort[0][direction]=asc';
 
-const apiUrl = `${BASE_URL}/${BASE_ID}/${TABLE_NAME}`;
+const apiUrl = `${BASE_URL}/${BASE_ID}/${TABLE_NAME}?${GRID_VIEW}`;
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
@@ -30,6 +34,20 @@ const App = () => {
       }
 
       const todoFromAPI = await response.json();
+      console.log('todoFromAPI: ', todoFromAPI.records);
+
+      const sortedTodos = todoFromAPI.records.sort((objectA, objectB) => {
+        const titleA = objectA.fields.title.toLowerCase();
+        const titleB = objectB.fields.title.toLowerCase();
+
+        if (titleA < titleB) {
+          return 1;
+        }
+        if (titleA > titleB) {
+          return -1;
+        }
+        return 0;
+      });
 
       const todos = todoFromAPI.records.map((todo) => {
         const todoItem = {
@@ -85,7 +103,7 @@ const App = () => {
               <div className={style.Content}>
                 <AddTodoForm onAddTodo={addTodo} />
                 {isLoading ? (
-                  <p>Loading...</p>
+                  <Loading></Loading>
                 ) : (
                   <TodoList
                     todoList={todoList}
